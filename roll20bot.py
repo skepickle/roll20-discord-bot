dc7e452b97c4b4de16f2968524f1b66112abefd4
Skepickle <skepickle@users.noreply.github.com>
2019-03-13 21:05:16 -0400

    Fixed syntax

--- roll20bot.py
+++ roll20bot.py
@@ -14,9 +14,16 @@ import asyncio
 
 # Options parsing
 
-token       = os.environ['DISCORD_TOKEN']     || ''
-journal     = os.environ['ROLL20_JOURNAL']    || ''
-chrome_path = os.environ['CHROMEDRIVER_PATH'] || ''
+token       = ''
+journal     = ''
+chrome_path = ''
+
+if ('DISCORD_TOKEN' in os.environ):
+    token       = os.environ['DISCORD_TOKEN']
+if ('ROLL20_JOURNAL' in os.environ):
+    journal     = os.environ['ROLL20_JOURNAL']
+if ('CHROMEDRIVER_PATH' in os.environ):
+    chrome_path = os.environ['CHROMEDRIVER_PATH']
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "ht:j:c:", ["token=", "journal=", "chrome="])
