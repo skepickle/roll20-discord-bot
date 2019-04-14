10e63bf003da16e9c7b26e7ee5e5098391bcf66a
Skepickle <skepickle@users.noreply.github.com>
2019-04-14 16:36:47 -0400

    Fixing derped syntax.

--- launcher.py
+++ launcher.py
@@ -333,10 +333,10 @@ def init(cogs, quiet):
                 click.echo(f'[{table.__module__}] No work needed for {table.__tablename__}.')
 
     click.echo(f'Wait for completion', err=False)
-    await asyncio.sleep(3)
+    asyncio.sleep(3)
     click.echo(f'db init complete', err=False)
     while True:
-        await asyncio.sleep(10)
+        asyncio.sleep(10)
 
 db.command(short_help='migrates the databases')
 @click.argument('cog', nargs=1, metavar='[cog]')
