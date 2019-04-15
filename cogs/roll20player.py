dfd6df23b816572543d45c3d52342c276a0462c0
Skepickle <skepickle@users.noreply.github.com>
2019-04-15 15:39:49 -0400

    Try and remove /player commands from Category in help

--- cogs/roll20player.py
+++ cogs/roll20player.py
@@ -70,7 +70,7 @@ def valid_roll20(argument):
         raise commands.BadArgument('An Roll20 user id must be an integer.')
     return val
 
-class Roll20Player(commands.Cog):
+class Roll20Player(commands.Cog, command_attrs=dict(cog_name=None)):
     def __init__(self, bot):
         self.bot = bot
 
