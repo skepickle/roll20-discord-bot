e378ab3e328984744f5a5b624bb5fa5f3e6a4c38
Skepickle <skepickle@users.noreply.github.com>
2019-04-16 22:05:56 -0400

    Syntax fix

--- cogs/roll20player.py
+++ cogs/roll20player.py
@@ -131,7 +131,7 @@ class Roll20Player(commands.Cog, name='Config'):
 
         if (record['roll20']):
             e.title = member.display_name
-            e.url = 'https://app.roll20.net/users/{}'.format(record['roll20']
+            e.url = 'https://app.roll20.net/users/{}'.format(record['roll20'])
             e.description = "Roll20 User ID is set."
             #e.add_field(name='Roll20 User ID', value='Set', inline=True)
             #e.set_author(name=member.display_name, url='https://app.roll20.net/users/{}'.format(record['roll20']), icon_url=member.avatar_url_as(format='png'))
