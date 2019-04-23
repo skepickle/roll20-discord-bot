fc8eb3bad0686351136e28e68cee6c6ec04e3ac5
Skepickle <skepickle@users.noreply.github.com>
2019-04-22 22:32:11 -0400

    try to fix help

--- cogs/roll20player.py
+++ cogs/roll20player.py
@@ -87,7 +87,7 @@ class Roll20Player(commands.Cog, name='Config'):
     """Handles players' information.
 
     This is information about IRL players."""
-
+    return
     if ctx.invoked_subcommand is None:
       await ctx.send_help('player')
 
@@ -157,7 +157,7 @@ class Roll20Player(commands.Cog, name='Config'):
 
     await ctx.db.execute(query, member.id, *fields.values())
 
-  @_player.group(name='set', invoke_without_command=True)
+  @_player.group(name='set')
   async def _set(self, ctx):
     """Sets a player's field value.
  
@@ -166,7 +166,7 @@ class Roll20Player(commands.Cog, name='Config'):
     - roll20"""
 
     if ctx.invoked_subcommand is None:
-      await ctx.send_help(_set)
+      await ctx.send_help("player")
 
   @_set.command(name='roll20')
   async def _set_roll20(self, ctx, id: valid_roll20, *, member: DisambiguateMember = None):
